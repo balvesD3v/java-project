150f5488173fe1aa9f58b4118f6afb67305711ad
balvesD3v <lpbalves7filho@gmail.com>
2024-04-08 12:32:39 -0300

    List data structure

---
+++ DataStructure.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Luiz");
+        nomes.add("Manu");
+
+        // for (String nome : nomes) {
+        // System.out.println(nome);
+        // }
+    
+        nomes.forEach(nome -> System.out.println(nome));
+    }
+}
